a78d6816097af78683cbd972e63111b227be12d3
Chadwick Stryker <chadwick@stryker5.org>
2024-05-24 16:08:23 -0700

    Updated after testing game piece launcher.

--- controls/motor_tester/robot.py
+++ controls/motor_tester/robot.py
@@ -5,7 +5,12 @@
 import wpilib
 import wpilib.drive
 
-from phoenix6 import hardware, configs
+from phoenix6.controls import DutyCycleOut
+from phoenix6.hardware import TalonFX
+from phoenix6.configs.talon_fx_configs import InvertedValue, NeutralModeValue, TalonFXConfiguration
+
+
+SPEED = 0.3
 
 
 class MyRobot(wpilib.TimedRobot):
@@ -14,29 +19,52 @@ class MyRobot(wpilib.TimedRobot):
         This function is called upon program startup and
         should be used for any initialization code.
         """
-        self.left_motor = wpilib.Talon(0)
-        self.right_motor = wpilib.Talon(1)
-        self.drive = wpilib.drive.DifferentialDrive(self.left_motor, self.right_motor)
-        self.stick = wpilib.Joystick(0)
-        self.timer = wpilib.Timer()
+        self.upper_left_motor = TalonFX(16)
+        self.lower_left_motor = TalonFX(17)
+        self.upper_right_motor = TalonFX(18)
+        self.lower_right_motor = TalonFX(19)
+
+        clockwise_config = TalonFXConfiguration()
+        clockwise_config.motor_output.with_neutral_mode(NeutralModeValue.COAST)
+        clockwise_config.feedback.sensor_to_mechanism_ratio = 1
+        clockwise_config.inverted = 1
+
+        # counter_clockwise_config = TalonFXConfiguration()
+        # counter_clockwise_config.motor_output.with_neutral_mode(NeutralModeValue.COAST)
+        # counter_clockwise_config.feedback.sensor_to_mechanism_ratio = 1
+        # counter_clockwise_config.inverted = 0
+
+        self.upper_left_motor.configurator.apply(clockwise_config)
+        self.lower_left_motor.configurator.apply(clockwise_config)
+        self.upper_right_motor.configurator.apply(clockwise_config)
+        self.lower_right_motor.configurator.apply(clockwise_config)
+
+        self.upper_left_request = DutyCycleOut(0.0)
+        self.upper_right_request = DutyCycleOut(0.0)
+        self.lower_left_request = DutyCycleOut(0.0)
+        self.lower_right_request = DutyCycleOut(0.0)
 
     def autonomousInit(self):
         """This function is run once each time the robot enters autonomous mode."""
-        self.timer.reset()
-        self.timer.start()
+        self.upper_left_motor.set_control(self.upper_left_request.with_output(-SPEED))
+        self.lower_left_motor.set_control(self.lower_left_request.with_output(SPEED))
+        self.upper_right_motor.set_control(self.upper_right_request.with_output(SPEED))
+        self.lower_right_motor.set_control(self.lower_right_request.with_output(-SPEED))
 
     def autonomousPeriodic(self):
         """This function is called periodically during autonomous."""
+        pass
 
-        # Drive for two seconds
-        if self.timer.get() < 2.0:
-            self.drive.arcadeDrive(-0.5, 0)  # Drive forwards at half speed
-        else:
-            self.drive.arcadeDrive(0, 0)  # Stop robot
+    def disabledInit(self):
+        """This function is called at the end of autonomous mode."""
+        self.upper_left_motor.set_control(self.upper_left_request.with_output(0.0))
+        self.lower_left_motor.set_control(self.lower_left_request.with_output(0.0))
+        self.upper_right_motor.set_control(self.upper_right_request.with_output(0.0))
+        self.lower_right_motor.set_control(self.lower_right_request.with_output(0.0))
 
     def teleopPeriodic(self):
         """This function is called periodically during operator control."""
-        self.drive.arcadeDrive(self.stick.getY(), self.stick.getX())
+        pass
 
 
 if __name__ == "__main__":
